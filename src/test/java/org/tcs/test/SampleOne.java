9d29caecfcfedfec0367c430fc01b4961f815e90
SaroSaroSaro <saravananhh4345@gmail.com>
2024-02-22 23:03:36 +0530

    Hii i am saravanan

---
+++ src/test/java/org/tcs/test/SampleOne.java
@@ -0,0 +1,15 @@
+package org.tcs.test;
+
+public class SampleOne {
+
+	private void welcome() {
+		System.out.println("Hii Hello Welcome");
+		
+	}
+
+	public static void main(String[] args) {
+		SampleOne s=new SampleOne();
+		s.welcome();
+	}
+	
+}
