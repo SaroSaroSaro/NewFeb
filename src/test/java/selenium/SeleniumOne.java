bd6d34a753f5b2f79e3e8c635aa5215c143aa05c
SaroSaroSaro <saravananhh4345@gmail.com>
2024-02-23 01:33:45 +0530

    good afternoon

---
+++ src/test/java/selenium/SeleniumOne.java
@@ -0,0 +1,17 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumOne {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+		
+	}
+	
+	
+	
+	
+}
